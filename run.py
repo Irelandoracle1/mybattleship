cf9eff46d4c65c7c2662815ac35c0830da67d4a7
Irelandoracle1 <nairajnr1@gmail.com>
2022-06-13 22:27:10 +0000

    created the battleship game class and also created the player and computer attributes and initialized them with an empty list. then created the init method

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Mybattleship_Game:
+    """
+    This is the battleship game class
+    it holds all the logic for the battleship game
+    """
+    player=[]
+    computer=[]
+
+    def __init__(self):
+        """
+        This concstructor method 
+        is used for adding values to boards
+        """
+
+
+
+
+
+
+
+
