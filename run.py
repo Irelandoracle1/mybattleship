79b17b846d147e4dcf4a4e0fc9b390592882603a
Irelandoracle1 <nairajnr1@gmail.com>
2022-06-14 00:13:14 +0000

    changed the user turn method to player turn then created the computer game turn method and called inside the player turn method. then added input validation check. then I added a welcome message before the game instance

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ class Mybattleship_Game:
         print("Computer Game Board")
         self.computer_board()
         for m in range(10):
-            self.user_turn()
+            self.player_turn()
             m+=1
 
     
@@ -55,14 +55,22 @@ class Mybattleship_Game:
         for the column
         """
         return random.randint(0, 5-1)
-    def user_turn(self):
+    def player_turn(self):
         """
         This method defines the logic for the users
         turn of play
         """
         while True:
-            guess_row=int(input("Guess a Row:"))
-            guess_column=int(input("Guess a Column:"))
+            try:
+              guess_row=int(input("Guess a Row:"))
+            except ValueError:
+                  print("You entered an invalid row value. You should choose integer values only")
+                  break
+            try:
+              guess_column=int(input("Guess a Column:"))
+            except ValueError:
+                  print("You entered an invalid row value. You should choose integer values only")
+                  break
             if guess_row==self.random_row() and guess_column==self.random_column():
                 self.computer[guess_row][guess_column]="$"
                 print("Yo! you made hit")
@@ -73,15 +81,38 @@ class Mybattleship_Game:
                 else:
                     self.computer[guess_row][guess_column]="X"
                     print(" Sorry! you missed the hit")
+            self.computer_turn()
             print("My Game Board")
             self.player_board()
             print("\n")
             print("Computer Game Board")
             self.computer_board()
+            
+
+    def computer_turn(self):
+        """
+        This method defines the logic for the computer
+        turn of play
+        """
+        guess_row=self.random_row()
+        guess_column=self.random_column()
+        game_alive=True
+        while game_alive:
+            if guess_row==self.random_row() and guess_column==self.random_column():
+                self.player[guess_row][guess_column]="$"
+                print("Hey! you were hit by the computer")
+            else:
+                self.player[guess_row][guess_column]="X"
+                print(" the computer missed the hit")
+            game_alive=False
+                
+            
+        
 
 
 
-
+print("Welcome to The Battleship Game")
+input("click enter to start playing!")
 my_game=Mybattleship_Game()
 
 
