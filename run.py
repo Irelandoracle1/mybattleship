93b6be7d996930c15ea76de82ba13b5644f7fd28
Irelandoracle1 <nairajnr1@gmail.com>
2022-06-16 00:58:30 +0000

    fixed indention errors and white spaces after testing with pep8

--- run.py
+++ run.py
@@ -18,7 +18,8 @@ class Mybattleship_Game:
             self.player.append(["~"]*5)
         for c in range(5):
             self.computer.append(["-"]*5)
-        player_name=input("Enter Your Name and Click Enter to Start Playing!\n")
+        player_name = input("Enter Your Name and Click Enter to  " +
+                            "Start Playing!\n")
         print(f"{player_name} Game Board")
         self.player_board()
         print("\n")
@@ -31,63 +32,69 @@ class Mybattleship_Game:
     def player_board(self):
         """
         This method creates the player board
-        from the player list already populated in 
+        from the player list already populated in
         init method
-        """ 
+        """
         for play in self.player:
             print(" ".join(play))
+
     def computer_board(self):
         """
         This method creates the computer board
-        from the computer list already populated in 
+        from the computer list already populated in
         init method
-        """ 
+        """
         for comp in self.computer:
             print(" ".join(comp))
+
     def random_row(self):
         """
         this returns random values between 0 and 4
         for the row
         """
         return random.randint(0, 5-1)
+
     def random_column(self):
         """
         this returns random values between 0 and 4
         for the column
         """
         return random.randint(0, 5-1)
+
     def player_turn(self):
         """
         This method defines the logic for the users
         turn of play
         """
-        v=1
-        while v<11:
+        v = 1
+        while v < 11:
             try:
-              guess_row=int(input("Guess a Row:"))
+                guess_row = int(input("Guess a Row:"))
             except ValueError:
-                  print("You entered an invalid row value. You should choose integer values only")
-                  continue
+                print("You entered an invalid row value." +
+                      "You should choose integer values only")
+                continue
             try:
-              guess_column=int(input("Guess a Column:"))
+                guess_column = int(input("Guess a Column:"))
             except ValueError:
-                  print("You entered an invalid row value. You should choose integer values only")
-                  continue
+                print("You entered an invalid row value." +
+                      "You should choose integer values only")
+                continue
 
-            if(guess_row<0 or guess_row>4) or (guess_column<0 or guess_column>4):
-                    print("Sorry! guessed out of range. choose values from 0 to 4")
-                    continue
+            if(guess_row < 0 or guess_row > 4) or (guess_column < 0 or
+                                                   guess_column > 4):
+                print("Sorry! guessed out of range. choose values from 0 to 4")
+                continue
 
-            elif (self.computer[guess_row][guess_column]=="X") or (self.computer[guess_row][guess_column]=="$"):
+            elif (self.computer[guess_row][guess_column] == "X") or (self.computer[guess_row][guess_column] == "$"):
                 print("You have guessed that already")
 
-            elif guess_row==self.random_row() and guess_column==self.random_column():
-                self.computer[guess_row][guess_column]="$"
+            elif (guess_row == self.random_row()) and (guess_column == self.random_column()):
+                self.computer[guess_row][guess_column] = "$"
                 print("Yeh! you made hit")
             else:
-                self.computer[guess_row][guess_column]="X"
+                self.computer[guess_row][guess_column] = "X"
                 print(" Sorry! you missed the hit")
-                    
             self.computer_turn()
             print(f'You have played {v} rounds out 10 rounds')
             print(f'SCORES For this Round: You:{self.game_score(self.computer)} | Computer: {self.game_score(self.player)}')
@@ -96,42 +103,42 @@ class Mybattleship_Game:
             print("\n")
             print("Computer Game Board")
             self.computer_board()
-            quit_game=input("Click any button and enter to continue or click X and enter to exit the game")
-            if quit_game=="x":
+            quit_game = input("Click any button and enter to continue or click X and enter to exit the game")
+            if quit_game == "x":
                 sys.exit()
-            if v>9:
+            if v > 9:
                 print(self.game_winner())
                 print(f'FINAL SCORE: You:{self.game_score(self.computer)} | Computer: {self.game_score(self.player)}')
                 print("GAME OVER!!!")
                 sys.exit()
 
-            v+=1
+            v += 1
 
     def computer_turn(self):
         """
         This method defines the logic for the computer
         turn of play
         """
-        guess_row=self.random_row()
-        guess_column=self.random_column()
-        game_alive=True
+        guess_row = self.random_row()
+        guess_column = self.random_column()
+        game_alive = True
         while game_alive:
-            if (self.player[guess_row][guess_column]=="X") or (self.player[guess_row][guess_column]=="$"):
+            if (self.player[guess_row][guess_column] == "X") or (self.player[guess_row][guess_column] == "$"):
                 print("computer had guessed that before")
 
-            elif guess_row==self.random_row() and guess_column==self.random_column():
-                self.player[guess_row][guess_column]="$"
+            elif guess_row == self.random_row() and guess_column == self.random_column():
+                self.player[guess_row][guess_column] = "$"
                 print("Hey! you were hit by the computer")
 
             else:
-                self.player[guess_row][guess_column]="X"
+                self.player[guess_row][guess_column] = "X"
                 print(" the computer missed the hit")
 
-            game_alive=False
-    
+            game_alive = False
+
     def game_score(self, game_board):
         """
-        This method is for summing up the player 
+        This method is for summing up the player
         and computer scores from their boards
         """
         scores = sum(s.count("$") for s in game_board)
@@ -141,22 +148,15 @@ class Mybattleship_Game:
         """
         This method is used for determining the game winner
         """
-        if self.game_score(self.computer)>self.game_score(self.player):
+        if self.game_score(self.computer) > self.game_score(self.player):
             message = "Hurray! You are the winner of the game"
-        elif self.game_score(self.player)>self.game_score(self.computer):
+        elif self.game_score(self.player) > self.game_score(self.computer):
             message = "Sorry! the computer won"
         else:
             message = "There is no winner. It is a draw"
         return message
-            
-        
-
 
 
 print("Welcome to The Battleship Game")
-my_game=Mybattleship_Game()
 
-
-
-
-
+my_game = Mybattleship_Game()
